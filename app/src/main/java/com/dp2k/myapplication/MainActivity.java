083233caa843f21cd333497c23ab1d1198eb534a
Daylin Pillay <daylin.pillay@gmail.com>
2021-02-02 18:33:31 -0700

    Hello World

---
+++ app/src/main/java/com/dp2k/myapplication/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dp2k.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity
+{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
